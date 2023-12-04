83d6a4fb3df0d254f70de505cb704bf3359664e7
SafakOnol <onolsafak@outlook.com>
2023-12-03 22:29:30 -0500

    create Unreal Project

---
+++ Source/CoopAdventureOnline/CoopAdventureOnlineGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CoopAdventureOnlineGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACoopAdventureOnlineGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACoopAdventureOnlineGameMode();
+};
+
+
+
